b6df4de38221d9353aad7c3254b517317ad8c6e2
Bardia <bardiafarokhsereshti@gmail.com>
2026-05-14 08:30:47 +0330

    fix no contact

--- app/modules/hilfen/handlers/stateful/house_news_admin_handlers.py
+++ app/modules/hilfen/handlers/stateful/house_news_admin_handlers.py
@@ -320,13 +320,6 @@ class AdminConfirmCallbackHandler(BaseHandler):
             main_message_id,
         )
 
-        await news_repo.update_news(
-            news_id=news_id,
-            main_channel_id=target_channel,
-            main_channel_message_id=main_message_id,
-            status="publishing",
-        )
-
         # ---- 4) Wait for auto‑forwarded comment ----
         mapping = await _wait_for_comment_mapping(target_channel, main_message_id)
         group_chat_id = None
@@ -352,6 +345,13 @@ class AdminConfirmCallbackHandler(BaseHandler):
                         group_chat_id=group_chat_id,
                         group_message_id=group_msg_id,
                     )
+
+        await news_repo.update_news(
+            news_id=news_id,
+            main_channel_id=target_channel,
+            main_channel_message_id=main_message_id,
+            status="publishing",
+        )
 
         # ---- 6) Post published comments in admin / hilfen / main groups ----
         comment_text = format_published_comment(news)
